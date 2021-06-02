238f1b3ce8ef8a1708eac006054e4ade0e8e8976
anjali0116 <84908662+anjali0116@users.noreply.github.com>
2021-06-02 21:39:06 +0530

    Update EmployeeSpringBoot1Application.java

--- src/main/java/com/hcl/employeedetails/EmployeeSpringBoot1Application.java
+++ src/main/java/com/hcl/employeedetails/EmployeeSpringBoot1Application.java
@@ -7,12 +7,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class EmployeeSpringBoot1Application extends SpringBootServletInitializer{
-@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-		return application.sources(EmployeeSpringBoot1Application.class);
-	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeSpringBoot1Application.class, args);
 	}
 
+
+@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(EmployeeSpringBoot1Application.class);
+	}
 }
